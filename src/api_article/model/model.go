51f5df5e3f3d639ab2c169845d9b0ea6924373f0
LIUZHAO <liuzhaomax@163.com>
2024-04-02 22:17:02 +0800

    blog

--- src/api_article/model/model.go
+++ src/api_article/model/model.go
@@ -6,13 +6,13 @@ import (
 
 type Article struct {
 	gorm.Model
-	Id        string `gorm:"index:idx_id;unique;varchar(20);not null;"`
+	ArticleID string `gorm:"index:idx_article_id;unique;varchar(20);not null;"`
 	Title     string `gorm:"index:idx_title;unique;varchar(150);not null;"`
 	Author    string `gorm:"varchar(30);not null;"`
 	Reference string `gorm:"varchar(300);"`
 	Link      string `gorm:"varchar(300);"`
-	View      uint   `gorm:"int;not null;default:0;"`
-	Like      uint   `gorm:"int;not null;default:0;"`
+	View      uint   `gorm:"number;not null;default:0;"`
+	Like      uint   `gorm:"number;not null;default:0;"`
 	Content   string `gorm:"text;"`
 }
 
--- src/api_article/schema/schema.go
+++ src/api_article/schema/schema.go
@@ -26,7 +26,7 @@ func MapArticle2ArticleRes(article *model.Article, tags []string) *ArticleRes {
 		deletedAt = article.DeletedAt.Time.String()
 	}
 	return &ArticleRes{
-		Id:        article.Id,
+		Id:        article.ArticleID,
 		CreatedAt: article.CreatedAt.String(),
 		UpdatedAt: article.UpdatedAt.String(),
 		DeletedAt: deletedAt,
