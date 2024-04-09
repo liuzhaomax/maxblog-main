16148f0b193f5145cea3afb7f9d650a98c00e84c
LIUZHAO <liuzhaomax@163.com>
2024-04-09 21:54:06 +0800

    model

--- src/api_article/model/model.go
+++ src/api_article/model/model.go
@@ -8,7 +8,7 @@ type Article struct {
 	gorm.Model
 	Id        string `gorm:"primarykey;index:idx_id;unique;varchar(20);not null;"`
 	Title     string `gorm:"index:idx_title;unique;varchar(150);not null;"`
-	Author    string `gorm:"varchar(30);not null;"`
+	Author    string `gorm:"varchar(30);not null;default:'马克西'"`
 	Reference string `gorm:"varchar(300);"`
 	Link      string `gorm:"varchar(300);"`
 	View      uint   `gorm:"number;not null;default:0;"`
